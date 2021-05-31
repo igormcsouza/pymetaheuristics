bfba9f3ac0ac9c18690c479c57f01634d9c382a0
Igor Souza <igormcsouza@gmail.com>
2021-05-31 19:03:21 -0300

    Add distance helper function

---
+++ pymetaheuristics/utils/distances.py
@@ -0,0 +1,12 @@
+from math import sqrt
+from typing import List
+
+
+def euclidian_distance(a: List[float], b: List[float]):
+    assert len(a) == len(b), "Length of tensor A has to be equal to tensor B."
+
+    distances = list()
+    for a_axis, b_axis in zip(a, b):
+        distances.append((a_axis - b_axis) ** 2)
+
+    return sqrt(sum(distances))
